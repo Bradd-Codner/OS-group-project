3a33ba3498d83a2298d5e89222492f1fcaea963d
Bradd-Codner <braddcodner0@gmail.com>
2025-11-23 07:52:59 -0500

    Add priority.hpp for preemptive scheduling

---
+++ priority.hpp
@@ -0,0 +1,10 @@
+#ifndef PRIORITY_HPP
+#define PRIORITY_HPP
+
+#include <vector>
+#include "process.hpp"
+
+// Preemptive Priority Scheduling (lower priority value = higher priority)
+void runPreemptivePriorityScheduling(std::vector<Process> &allProcesses);
+
+#endif
