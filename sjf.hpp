aafd4268bd0ce4a726ddac65931416c97e7bbdbe
Bradd-Codner <braddcodner0@gmail.com>
2025-11-23 09:19:24 -0500

    Update sjf.hpp

--- sjf.hpp
+++ sjf.hpp
@@ -1,10 +1,10 @@
+// sjf.hpp
 #ifndef SJF_HPP
 #define SJF_HPP
 
 #include <vector>
 #include "process.hpp"
 
-// Non-preemptive Shortest Job First
-void runSJF(std::vector<Process> &processes);
+void runSJF(std::vector<Process> &allProcesses);
 
 #endif
