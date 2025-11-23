0f0513901a50f4f05ea1e5ba4b8ca7c31a7ce437
Bradd-Codner <braddcodner0@gmail.com>
2025-11-22 22:34:19 -0500

    Create mlq_scheduler.hpp with scheduling function
    Add header file for Multi-Level Queue Scheduling

--- mlq_scheduler.hpp
+++ mlq_scheduler.hpp
@@ -1,1 +1,13 @@
+#ifndef MLQ_SCHEDULER_HPP
+#define MLQ_SCHEDULER_HPP
+
+#include <vector>
+#include "process.hpp"
+
+// Multi-Level Queue Scheduling:
+// - Foreground queue: Round Robin
+// - Background queue: FCFS
+void simulateMLQScheduling(std::vector<Process>& processes, int foregroundQuantum);
+
+#endif
 
